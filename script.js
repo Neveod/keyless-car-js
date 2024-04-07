68047acc95183fb57a5ca12a7efc6feb61bb84b0
Neveod <neve@ravenbay.com>
2024-04-07 14:03:08 -0600

    Connected the js to the button and centered text

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function checkID() {
-  age = prompt("How olde are you");
+function checkID(props) {
+  age = prompt("How old are you");
+  if (Number(age) < 18) {
+    alert("Sorry, you are too young to drive this car. Powering off");
+  } else if (Number(age) > 18) {
+    alert("Powering On. Enjoy the ride!");
+  } else if (Number(age) === 18) {
+    alert("Congratulations on your first year of driving. Enjoy the ride!");
+  }
 }
+
+let driveButton = document.querySelector("button");
+driveButton.addEventListener("click", checkID);
